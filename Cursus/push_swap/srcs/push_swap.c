cf6b6d63d7b936ccd08fcc89a08af68586328f3e
Scott WILLIS <swillis@e1r8p6.42.fr>
2022-01-20 15:53:00 +0100

    First time algo funcitonning but doesnt sort at all lol - making good progress :)

--- Cursus/push_swap/srcs/push_swap.c
+++ Cursus/push_swap/srcs/push_swap.c
@@ -6,54 +6,55 @@
 /*   By: swillis <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/17 18:24:44 by swillis           #+#    #+#             */
-/*   Updated: 2022/01/20 15:14:46 by swillis          ###   ########.fr       */
+/*   Updated: 2022/01/20 15:50:24 by swillis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
 
-int	stack_sortpush(t_node **stack, t_node **stack2, int *ptr)
+int	stack_sortpush(t_node **stack, t_node **stack2, int sets)
 {
 	t_node	*elem;
 	t_node	*next;
-	int		sets;
 
-	sets = *ptr;
 	elem = *stack;
 	next = elem->next;
 	if (stack_size(stack) > 1)
 	{
-		if (elem->val > next->val)
+		if (elem->val < next->val)
 			op_sa(stack);
 		op_pb(stack, stack2);
 		sets--;
 	}
 	op_pb(stack, stack2);
-	*ptr = sets;
-	return (0);
+	stack_print(stack);
+	return (sets);
 }
 
 int run_algo(t_node **stack)
 {
 	t_node	**stack2;
-	int		*tracker;
 	int		sets;
-
+	
 	// https://stackoverflow.com/questions/21897184/using-stacks-for-a-non-recursive-mergesort
+	printf("============= START =============\n");
 	stack_print(stack);
+	printf("=================================\n");
+	*stack2 = 0;
 	sets = stack_size(stack);
-	printf("%d\n", sets);
-	*tracker = sets;
-	printf("%d\n", *tracker);
-	while (*tracker > 1)
+	while (sets > 1)
 	{
-		stack_print(stack);
+		printf("============ STACK A ==================\n");
 		while (stack_size(stack) > 0)
-			stack_sortpush(stack, stack2, tracker);
+			sets = stack_sortpush(stack, stack2, sets);
+		printf("============ STACK B ==================\n");
 		while (stack_size(stack2) > 0)
-			stack_sortpush(stack2, stack, tracker);
+			sets = stack_sortpush(stack2, stack, sets);
 	}
+	printf("============= END =============\n");
+	stack_print(stack);
+	printf("=================================\n");
 	return (0);
 }
 
@@ -70,9 +71,8 @@ int	main(int ac, char **av)
 		stack = stack_init(array, ac - 1);
 		if (!stack)
 			return (1);
-		stack_print(stack);
-	//	if (run_algo(stack))
-	//		return (1);
+		if (run_algo(stack))
+			return (1);
 	}
 	return (0);
 }
