ae731f160edbd036254a0cfe827cd7a18f3241ed
hopscott <scott.willis2706@gmail.com>
2022-07-18 01:32:10 +0200

    need to work out why hangs on reaper thread

--- Cursus/philo/attempt3/philo/actions.c
+++ Cursus/philo/attempt3/philo/actions.c
@@ -6,7 +6,7 @@
 /*   By: swillis <swillis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 14:54:11 by swillis           #+#    #+#             */
-/*   Updated: 2022/07/17 23:06:33 by swillis          ###   ########.fr       */
+/*   Updated: 2022/07/18 01:31:43 by swillis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,10 @@ int	goto_sleep(t_philo *philo)
 {
 	int	time_to_sleep;
 
+	time_to_sleep = philo->vars->time_to_sleep;
 	if (is_alive(philo))
 	{
 		state_change(philo, SLEEPING, "is sleeping");
-		pthread_mutex_lock(&philo->lock);
-		time_to_sleep = philo->vars->time_to_sleep;
-		pthread_mutex_unlock(&philo->lock);
 		usleep(time_to_sleep * 1000);
 		return (1);
 	}
@@ -42,18 +40,16 @@ void	eat(t_philo *philo)
 {
 	int	time_to_eat;
 
+	time_to_eat = philo->vars->time_to_eat;
 	if (is_alive(philo))
 	{
 		pthread_mutex_lock(&philo->lock);
 		philo->time_of_last_meal = timestamp_ms();
 		philo->n_meals += 1;
-		time_to_eat = philo->vars->time_to_eat;
 		pthread_mutex_unlock(&philo->lock);
 		if (is_alive(philo))
 			usleep(time_to_eat * 1000);
 	}
-	pthread_mutex_unlock(philo->fk_left);
-	pthread_mutex_unlock(philo->fk_right);
 }
 
 int	goto_eat(t_philo *philo)
@@ -67,26 +63,25 @@ int	goto_eat(t_philo *philo)
 			pthread_mutex_lock(philo->fk_right);
 			if (is_alive(philo))
 			{
+				state_change(philo, HUNGRY, "has taken a fork");
 				state_change(philo, EATING, "is eating");
 				eat(philo);
-				if (is_alive(philo))
-					return (1);
-				else
-					return (0);
 			}
 			pthread_mutex_unlock(philo->fk_right);
 		}
 	}
 	pthread_mutex_unlock(philo->fk_left);
+	if (is_alive(philo))
+		return (1);
 	return (0);
 }
 
 void	check_soul(t_philo *philo, t_reaper *reaper)
 {
-	int	n_meals_needed;
-	int	n_meals;
-	int	time_of_last_meal;
 	int	time_to_die;
+	int	n_meals_needed;
+	int	time_of_last_meal;
+	int	n_meals;
 
 	time_to_die = reaper->vars->time_to_die;
 	n_meals_needed = reaper->vars->n_meals_needed;
