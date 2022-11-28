3cfdf434cf060abce786065b1d719b558f6d07aa
hopscott <scott.willis2706@gmail.com>
2022-11-28 17:08:56 +0100

    MOD07

--- Cursus/cpp/Module07/attempt1/ex02/includes/Array.hpp
+++ Cursus/cpp/Module07/attempt1/ex02/includes/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: swillis <swillis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 15:45:52 by swillis           #+#    #+#             */
-/*   Updated: 2022/11/28 00:35:32 by swillis          ###   ########.fr       */
+/*   Updated: 2022/11/28 15:05:00 by swillis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 #define RED			"\x1b[31m"
 #define GREEN		"\x1b[32m"
 #define YELLOW		"\x1b[33m"
+#define BLUE		"\x1b[34m"
 #define RESET		"\x1b[0m"
 
 template< typename T >
@@ -29,30 +30,99 @@ class Array {
 		// --------------- CONSTRUCTOR / DESTRUCTOR ---------------
 	
 		// Default Constructor
-		Array( void );
+		Array<T>( void ) : _n(0), _arr(NULL)
+		{
+			return;
+		}
 
 		// Parametric Constructor
-		Array( const unsigned int n );
+		Array<T>( const unsigned int n ): _n(n), _arr(NULL)
+		{
+			if (n > 0)
+			{
+				_arr = 	new T[_n];
+				for (unsigned int i=0; i<_n; ++i)
+				{
+					_arr[i] = 0;
+				}
+			}
+
+			return;
+		}
 
 		// Copy Constructor
-		Array( Array const & src );
+		Array<T>( Array<T> const & src ): _n(src._n), _arr(NULL)
+		{
+			if (_n > 0)
+			{
+				_arr = 	new T[_n];
+				
+				for (unsigned int i=0; i<_n; ++i)
+				{
+					_arr[i] = src._arr[i];
+				}
+			}
+			
+			return;
+		}
 	
 		// Destructor
-		~Array( void );
+		~Array<T>( void )
+		{
+			if (_arr)
+			{
+				delete [] _arr;
+			}
+			
+			return;
+		}
 	
 		// --------------- OPERATOR OVERLOADS ---------------
 	
 		// Copy Assignment Operator Overload
-		Array &	operator=(const Array & rhs);
+		Array<T> &	operator=(const Array<T> & rhs)
+		{
+
+			if (this != &rhs)
+			{
+				_n = rhs._n;
+				_arr = 	NULL;
+
+				if (_n > 0)
+				{
+					_arr = 	new T[_n];
+					
+					for (unsigned int i=0; i<_n; ++i)
+					{
+						_arr[i] = rhs._arr[i];
+					}
+				}
+			}
+
+			return (*this);
+		}
+
+		// Subscript Operator Overload
+		T &	operator[](const unsigned int pos)
+		{
+			if ((!_arr) || (pos >= _n))
+				throw std::exception();
+
+			return _arr[pos];
+		}
 
 		// --------------- FUNCTIONS ---------------
 	
 		// Member functions
+		unsigned int	size( void ) const
+		{
+			return _n;
+		}
 
 	private:
 
-		unsigned int	n;
-		T*				_arr;
+		unsigned int	_n;
+		T *				_arr;
 
 };
 
